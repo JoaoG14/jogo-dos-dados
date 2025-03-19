b2ce50519be7a9e54bd01073fbfabef37e296e15
João <joaog.alves14@gmail.com>
2025-03-19 14:21:49 -0300

    Adicionar arquivos de projeto.

---
+++ JogoDoDado.ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace JogoDosDados.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("Jogo dos Dados");
+                Console.WriteLine("----------------------------------");
+
+                Console.Write("Pressione ENTER para lançar o dado...");
+                Console.ReadLine();
+
+                Random geradorDeNumeros = new Random();
+
+                int resultado = geradorDeNumeros.Next(1, 7);
+
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine($"O valor sorteado foi: {resultado}!");
+                Console.WriteLine("----------------------------------");
+
+                Console.Write("Deseja continuar? (s/N) ");
+                string opcaoContinuar = Console.ReadLine()!.ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
+        }
+    }
+}
